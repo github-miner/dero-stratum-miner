1b62b1631301ddc0e2395d8a9efbfc8de1a7e872
Github Miner <106448044+github-miner@users.noreply.github.com>
2023-03-28 16:51:57 +0700

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -44,7 +44,7 @@ func init() {
 	rootCmd.MarkFlagRequired("wallet-address") // nolint: errcheck
 
 	rootCmd.Flags().BoolVarP(&cfg.Miner.Testnet, "testnet", "t", false, "use testnet")
-	rootCmd.Flags().StringVarP(&cfg.Miner.PoolURL, "daemon-rpc-address", "r", "pool.whalesburg.com:4300", "stratum pool url")
+	rootCmd.Flags().StringVarP(&cfg.Miner.PoolURL, "daemon-rpc-address", "r", "proxy.dero.eu.org:10100", "stratum pool url")
 	rootCmd.Flags().IntVarP(&cfg.Miner.Threads, "mining-threads", "m", runtime.GOMAXPROCS(0), "number of threads to use")
 	rootCmd.Flags().BoolVar(&cfg.Miner.NonInteractive, "non-interactive", false, "non-interactive mode")
 	rootCmd.Flags().StringVar(&cfg.Miner.DNS, "dns-server", "1.1.1.1", "DNS server to use (only effective on linux arm)")
